ee70e0d22dd9ff721c2874d7fb5952b1f83e1b77
Alexandr Rebikov <rebikovlk@mail.ru>
2022-01-14 15:42:52 +0700

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,31 @@
-{
-  "extends": "airbnb-base",
-  "env": {
-     "es2020": true,
-     "browser": true,
-     "jest": true,
-     "worker": true
+// eslint-disable-next-line import/no-unresolved
+const standardRestrictedGlobals = require('eslint-restricted-globals');
+
+const noRestrictedGlobals = ['error', 'isNaN', 'isFinite'].concat(standardRestrictedGlobals);
+const noRestrictedGlobalsWorker = noRestrictedGlobals.filter((o) => o !== 'self');
+
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    es2020: true,
+    browser: true,
+    jest: true,
+    worker: true,
   },
-  "rules": {
-    "no-restricted-syntax": [
-      "error",
-      "LabeledStatement",
-      "WithStatement"
-    ]
-  }
-}
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-restricted-globals': noRestrictedGlobals,
+  },
+  overrides: [
+    {
+      files: ['*-worker.js'],
+      rules: {
+        'no-restricted-globals': noRestrictedGlobalsWorker,
+      },
+    },
+  ],
+};
